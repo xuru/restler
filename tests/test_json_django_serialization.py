e15617f6b1aa55baea4a2c4941a9bac7070db09e
Brian Michelich <brian@michelich.com>
2012-10-26 08:35:24 -0500

    remaining django field json tests

--- tests/test_json_django_serialization.py
+++ tests/test_json_django_serialization.py
@@ -2,7 +2,7 @@ from env_setup import setup_django; setup_django()
 
 import json
 
-from datetime import datetime
+from datetime import datetime, time
 from unittest import TestCase
 
 from django.db import models, connection
@@ -98,7 +98,7 @@ class TestDjangoUnsupportedFields(TestCase):
             to_json(Model1.objects.all(), strategy)
 
 
-class TestJsonSerialization(TestCase):
+class TestDjangoFieldJsonSerialization(TestCase):
     def setUp(self):
         connection.creation.create_test_db(0, autoclobber=True)
         install_model(Model1)
@@ -111,7 +111,14 @@ class TestJsonSerialization(TestCase):
             email='test@domain.com',
             _float=9.5,
             integer=4,
-            ip_address='8.8.1.1.'
+            ip_address='8.8.1.1.',
+            null_boolean=None,
+            positive_int=100,
+            positive_small_int=1,
+            slug='Slug Field',
+            small_int=1,
+            text='More Text',
+            url='www.google.com'
         )
         self.model1.save()
         self.strategy = ModelStrategy(Model1, include_all_fields=False)
@@ -193,6 +200,54 @@ class TestJsonSerialization(TestCase):
 
     def test_ip_address_field(self):
         field = 'ip_address'
+        strategy = self.strategy.include(field)
+        sj = json.loads(to_json(Model1.objects.get(pk=self.model1.id), strategy))
+        self.assertEqual(sj.get(field), getattr(self.model1, field))
+
+    def test_null_boolean_field(self):
+        field = 'null_boolean'
+        strategy = self.strategy.include(field)
+        sj = json.loads(to_json(Model1.objects.get(pk=self.model1.id), strategy))
+        self.assertEqual(sj.get(field), getattr(self.model1, field))
+
+    def test_positive_integer_field(self):
+        field = 'positive_int'
+        strategy = self.strategy.include(field)
+        sj = json.loads(to_json(Model1.objects.get(pk=self.model1.id), strategy))
+        self.assertEqual(sj.get(field), getattr(self.model1, field))
+
+    def test_positive_small_integer_field(self):
+        field = 'positive_small_int'
+        strategy = self.strategy.include(field)
+        sj = json.loads(to_json(Model1.objects.get(pk=self.model1.id), strategy))
+        self.assertEqual(sj.get(field), getattr(self.model1, field))
+
+    def test_slug_field(self):
+        field = 'slug'
+        strategy = self.strategy.include(field)
+        sj = json.loads(to_json(Model1.objects.get(pk=self.model1.id), strategy))
+        self.assertEqual(sj.get(field), getattr(self.model1, field))
+
+    def test_small_integer_field(self):
+        field = 'small_int'
+        strategy = self.strategy.include(field)
+        sj = json.loads(to_json(Model1.objects.get(pk=self.model1.id), strategy))
+        self.assertEqual(sj.get(field), getattr(self.model1, field))
+
+    def test_text_field(self):
+        field = 'text'
+        strategy = self.strategy.include(field)
+        sj = json.loads(to_json(Model1.objects.get(pk=self.model1.id), strategy))
+        self.assertEqual(sj.get(field), getattr(self.model1, field))
+
+    def test_time_field(self):
+        field = '_time'
+        strategy = self.strategy.include(field)
+        sj = json.loads(to_json(Model1.objects.get(pk=self.model1.id), strategy))
+        self.assertEqual(sj.get(field), time.strftime(getattr(self.model1, field), '%H:%M:%S'))
+
+    def test_url_field(self):
+        field = 'url'
         strategy = self.strategy.include(field)
         sj = json.loads(to_json(Model1.objects.get(pk=self.model1.id), strategy))
         self.assertEqual(sj.get(field), getattr(self.model1, field))
